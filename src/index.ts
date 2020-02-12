5a565e9db95199424763ace388500e20feb5cdad
zhanglainghao <15821881675@163.com>
2020-02-12 17:12:18 +0800

    编写命令

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('GG');
+import commander from 'commander';
+import colors from 'colors';
+const program = new commander.Command();
+program
+  .version('0.0.1')
+  .option('-c, --city <type>', 'Add city name')
+  .parse(process.argv);
+
+if (process.argv.slice(2).length === 0) {
+  program.outputHelp(colors.red);
+  process.exit();
+}
